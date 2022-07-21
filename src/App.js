f5100828dadc0c8813636754b93d30a59f73c579
cifong <raprope@gmail.com>
2022-07-21 21:46:46 +0800

    git init

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './assets/App.css';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from './components/home/home';
+import Skills from './components/skill/skills';
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/skill">技能</Link>
+            </li>
+          </ul>
+
+          <Routes>
+            <Route path="/skill" element={<Skills />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
