cef8226a8f65deee1d1ac64c510e6dbb96bae7a7
cifong <raprope@gmail.com>
2022-08-09 19:08:23 +0800

    character list css draft

--- src/pages/characters/characters.js
+++ src/pages/characters/characters.js
@@ -2,6 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Resources from "components/resource/resource";
 export default function Characters(props) {
+    const data = new Array(10).fill(0).map((v) => {
+        console.log(v)
+        return <div className="character-item">555</div>;
+    })
+    console.log(data)
     return (
         <div className="character-wrapper">
             <div className="homeconfig">
@@ -14,8 +19,11 @@ export default function Characters(props) {
             <div className="character-detail"> 
                 123
             </div>
-            <div className="character-list">
-                456
+            <div className="character-nav">
+                <div className="character-pick"></div>
+                <div className="character-list">
+                    {data}
+                </div>
             </div>
         </div>
     );
