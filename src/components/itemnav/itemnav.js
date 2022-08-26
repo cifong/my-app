86a9cee293ddb28964244ce69b3133957480277a
cifong <raprope@gmail.com>
2022-08-26 22:02:10 +0800

    item lock and return btn draft

--- src/components/itemnav/itemnav.js
+++ src/components/itemnav/itemnav.js
@@ -6,7 +6,10 @@ export default function Itemnav(props) {
     };
     return (
         <div className="item-nav">
-            <div className="item-pick" style={pickStyle}></div>
+            <div className="item-pick" style={pickStyle}>
+                <div className="item-pick-lock"></div>
+                <div className="item-pick-return"></div>
+            </div>
             <div className="item-list">
                 {props.data}
             </div>
